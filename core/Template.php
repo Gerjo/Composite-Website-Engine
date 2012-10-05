7e6020f589ad921395401b2ace9b65a88f5adeb3
Gerjo <gerjoo@gmail.com>
2012-10-05 11:28:57 +0200

    altered parsing to be more robust

--- core/Template.php
+++ core/Template.php
@@ -3,50 +3,54 @@
 final class Template extends Composite {
     private $fileName;
     private $html;
-    
+
     public function __construct($templateFile = null) {
         parent::__construct();
-        
+
         $this->fileName = $templateFile;
         $this->html     = null;
-        
+
         if($templateFile !== null) {
             $this->html = file_get_contents($templateFile);
-            
+
             if(false === $this->html) {
                 throw new Exception("Unable to open template file for reading.");
             }
         }
     }
-    
+
     // TODO variable calling and existance checking.
     private function injectComponent(Composite $composite) {
-        preg_match_all('#\[([a-z]{1,})\]#i', $this->html, $keys);
-        
+        preg_match_all('#\[get([a-z]{1,})\]#i', $this->html, $keys);
+
         foreach($keys[1] as $k => $v) {
-            $data       = $composite->$v();
+            $method = $v;
+
+            //if(is_callable($method))
+
+            $data       = $composite->$method();
             $this->html = str_replace($keys[0][$k], $data, $this->html);
         }
-        
+
         return $this;
     }
-    
+
     public function getHtml() {
         return $this->html;
     }
-    
+
     public function setHtml($html) {
         $this->html = $html;
         return $this;
     }
-    
+
     public function __clone() {
         $clone = new Template();
         $clone->setHtml($this->getHtml());
-        
+
         return $clone;
     }
-    
+
     public function onRender(ClientRequest $request, Document $document) {
         $this->injectComponent($this->getParent());
         $document->addTemplate($this);
