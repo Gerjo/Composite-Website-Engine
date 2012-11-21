7fa52ba81f04d4b9de0d812c5f7f2d2a7d1c1c6a
Gerjo Meier <gerjoo@gmail.com>
2012-11-21 14:48:41 +0100

    Added initial iterator support. It's probably broken for nested iterators.

--- core/Template.php
+++ core/Template.php
@@ -19,22 +19,21 @@ final class Template extends Composite {
         }
     }
 
-    // TODO variable calling and existance checking.
-    private function injectComponent(Composite $composite) {
-        preg_match_all('#\[get([a-z]{1,})\]#i', $this->html, $keys);
+    private function apply($html, $composite) {
+        preg_match_all('#\[get([a-z]{1,})\]#i', $html, $keys);
 
         foreach($keys[1] as $k => $v) {
             $method = "get" . $v;
 
             if(is_callable(array(get_class($composite), $method))) {
-                $data       = $composite->$method();
-                $this->html = str_replace($keys[0][$k], $data, $this->html);
+                $data = $composite->$method();
+                $html = str_replace($keys[0][$k], $data, $html);
             }
         }
 
         foreach(array("has", "can", "is", "enable", "should") as $prefix) {
 
-            preg_match_all('#\[' . $prefix . '([a-z]{1,})\]#i', $this->html, $keys);
+            preg_match_all('#\[' . $prefix . '([a-z]{1,})\]#i', $html, $keys);
 
             foreach($keys[1] as $k => $v) {
                 $method = $prefix . $v;
@@ -42,18 +41,47 @@ final class Template extends Composite {
                 $close  = '[/' . $method . ']';
                 $regex  = '#' . preg_quote($open) . '(.+?)' . preg_quote($close) . '#s';
                 
-                while(preg_match($regex, $this->html, $htmlMatches) == 1) {
+                while(preg_match($regex, $html, $htmlMatches) == 1) {
                     if(is_callable(array(get_class($composite), $method))) {
                         if(true === $composite->$method()) {
-                            $this->html = preg_replace($regex,  $htmlMatches[1], $this->html);
+                            $html = preg_replace($regex,  $htmlMatches[1], $html);
                         } else {
-                            $this->html = preg_replace($regex, "", $this->html);
+                            $html = preg_replace($regex, "", $html);
                         }
                     }
                 }
             }
         }
+    
+        return $html;
+    }
+    
+    private function injectComponent(Composite $composite) {
+        preg_match_all('#\[iterator\ ([a-z]{1,})\]#i', $this->html, $keys);
 
+        foreach($keys[1] as $k => $v) {
+            if(empty($v)) continue;
+            
+            $method = $v;
+            $open   = '[iterator ' . $method . ']';
+            $close  = '[/iterator ' . $method . ']';
+            $regex  = '#' . preg_quote($open) . '(.+?)' . preg_quote($close) . '#s';
+            
+            preg_match($regex, $this->html, $out);
+            $source    = $out[1];
+            $formatted = "";
+            
+            $iterator  = $composite->$method();
+            
+            foreach($iterator as $k => $v) {
+                $formatted .= $this->apply($source, $v);
+            }
+            
+            $this->html = str_ireplace($out[0], $formatted, $this->html);
+        }
+        
+        $this->html = $this->apply($this->html, $composite);
+ 
         return $this;
     }
 
